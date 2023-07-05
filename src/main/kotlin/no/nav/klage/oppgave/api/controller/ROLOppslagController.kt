987adaf7a844f3befc825a42bb6fffe49b3fb4dd
Andreas Jonsson <andreas.jonsson@nav.no>
2023-07-05 11:11:53 +0200

    ROL
    Co-authored-by: Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>

---
+++ src/main/kotlin/no/nav/klage/oppgave/api/controller/ROLOppslagController.kt
@@ -0,0 +1,50 @@
+package no.nav.klage.oppgave.api.controller
+
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import no.nav.klage.oppgave.api.view.Saksbehandlere
+import no.nav.klage.oppgave.api.view.SearchROLInput
+import no.nav.klage.oppgave.config.SecurityConfiguration
+import no.nav.klage.oppgave.repositories.InnloggetAnsattRepository
+import no.nav.klage.oppgave.service.SaksbehandlerService
+import no.nav.klage.oppgave.util.getLogger
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.springframework.core.env.Environment
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@ProtectedWithClaims(issuer = SecurityConfiguration.ISSUER_AAD)
+@RestController
+@Tag(name = "Medunderskriver")
+class ROLOppslagController(
+    private val saksbehandlerService: SaksbehandlerService,
+    private val environment: Environment,
+    private val innloggetAnsattRepository: InnloggetAnsattRepository
+) {
+
+    companion object {
+        @Suppress("JAVA_CLASS_ON_COMPANION")
+        private val logger = getLogger(javaClass.enclosingClass)
+    }
+
+    @Operation(
+        summary = "Hent ROLs for en ansatt",
+        description = "Henter alle ROLs som kan brukes for en gitt ytelse og fnr."
+    )
+    @PostMapping(
+        "/search/rol",
+        produces = ["application/json"]
+    )
+    fun getROLForFnr(
+        @RequestBody input: SearchROLInput,
+    ): Saksbehandlere {
+        val innloggetSaksbehandlerNavIdent = innloggetAnsattRepository.getInnloggetIdent()
+        logger.debug("getROLForFnr is requested by $innloggetSaksbehandlerNavIdent")
+        return saksbehandlerService.getROLList(
+            fnr = input.fnr
+        )
+    }
+
+}

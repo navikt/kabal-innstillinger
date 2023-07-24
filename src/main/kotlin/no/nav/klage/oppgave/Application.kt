e1762cb68de08965aab4509cc88c30dfa1740723
Øyvind Wedøe <oyvind.norsted.wedoe@nav.no>
2023-07-24 10:23:54 +0200

    Enable scheduling.

--- src/main/kotlin/no/nav/klage/oppgave/Application.kt
+++ src/main/kotlin/no/nav/klage/oppgave/Application.kt
@@ -3,8 +3,10 @@ package no.nav.klage.oppgave
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication(exclude = [ErrorMvcAutoConfiguration::class])
+@EnableScheduling
 class Application
 
 fun main() {

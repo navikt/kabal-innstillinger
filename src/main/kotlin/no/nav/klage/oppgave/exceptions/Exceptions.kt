91f62b0c645ea6b44311234e8b875142252ae85f
Andreas Jonsson <andreas.jonsson@nav.no>
2026-03-04 15:53:18 +0100

    Handle exceptions.

--- src/main/kotlin/no/nav/klage/oppgave/exceptions/Exceptions.kt
+++ src/main/kotlin/no/nav/klage/oppgave/exceptions/Exceptions.kt
@@ -2,8 +2,6 @@ package no.nav.klage.oppgave.exceptions
 
 class MissingTilgangException(msg: String) : RuntimeException(msg)
 
-class EnhetNotFoundForSaksbehandlerException(msg: String) : RuntimeException(msg)
-
 class AbbreviationAlreadyExistsException(msg: String) : RuntimeException(msg)
 
 class IllegalInputException(msg: String) : RuntimeException(msg)

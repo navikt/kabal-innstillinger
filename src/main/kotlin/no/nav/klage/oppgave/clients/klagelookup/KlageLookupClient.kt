7a8b1547a7c16d70e344657641353af8cbbfb88a
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2026-04-14 13:14:09 +0200

    Use correct token.

--- src/main/kotlin/no/nav/klage/oppgave/clients/klagelookup/KlageLookupClient.kt
+++ src/main/kotlin/no/nav/klage/oppgave/clients/klagelookup/KlageLookupClient.kt
@@ -244,6 +244,7 @@ class KlageLookupClient(
     @Retryable
     fun getPerson(fnr: String, sak: Sak?): PersonResponse {
         return runWithTimingAndLogging {
+            val token = getCorrectBearerToken()
             klageLookupWebClient.post()
                 .uri("/person")
                 .bodyValue(
@@ -254,7 +255,7 @@ class KlageLookupClient(
                 )
                 .header(
                     HttpHeaders.AUTHORIZATION,
-                    "Bearer ${tokenUtil.getAppAccessTokenWithKlageLookupScope()}",
+                    token,
                 )
                 .retrieve()
                 .onStatus(HttpStatusCode::isError) { response ->

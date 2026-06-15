47585ad03094931a3289b1320f7d53bb2b388d53
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2026-06-15 14:33:18 +0200

    Include ansattEnhetId when getting saksbehandler and medunderskriver lists.

--- src/main/kotlin/no/nav/klage/oppgave/api/view/SearchResponse.kt
+++ src/main/kotlin/no/nav/klage/oppgave/api/view/SearchResponse.kt
@@ -2,11 +2,15 @@ package no.nav.klage.oppgave.api.view
 
 data class Saksbehandlere(val saksbehandlere: List<Saksbehandler>)
 
-data class Saksbehandler(val navIdent: String, val navn: String)
+data class Saksbehandler(
+    val navIdent: String,
+    val navn: String,
+    val ansattEnhetId: String,
+)
 
 data class MedunderskrivereForYtelse(
     //TODO: Dette er ytelseId, bør få nytt navn.
     val ytelse: String,
-    val medunderskrivere: List<Saksbehandler>
+    val medunderskrivere: List<Saksbehandler>,
 )
 

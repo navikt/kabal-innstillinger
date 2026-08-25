a62b950153f043b14d05ddd322cebf483ef0f523
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2026-08-25 15:09:43 +0200

    Kodeverk 3.3.18.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ val springMockkVersion = "5.0.1"
 val springDocVersion = "3.1.0"
 val testContainersVersion = "2.0.5"
 val ehcacheVersion = "3.12.0"
-val kodeverkVersion = "3.3.14"
+val kodeverkVersion = "3.3.18"
 val shedlockVersion = "7.8.0"
 
 plugins {

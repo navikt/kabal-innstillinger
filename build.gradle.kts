721e13851e471a8c713d31dbee53ae30cb421a45
Andreas Jonsson <andreas.jonsson@nav.no>
2026-08-28 11:30:01 +0200

    Update kodeverkVersion to 3.3.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ val springMockkVersion = "5.0.1"
 val springDocVersion = "3.1.0"
 val testContainersVersion = "2.0.5"
 val ehcacheVersion = "3.12.0"
-val kodeverkVersion = "3.3.18"
+val kodeverkVersion = "3.3.20"
 val shedlockVersion = "7.8.0"
 
 plugins {
